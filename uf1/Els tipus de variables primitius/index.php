9251a4d105e612525f17c2913047a8aa3a315a62
Nil Pujol <nilpujolporta@gmail.com>
2022-09-15 17:49:20 +0200

    Practiques simples

---
+++ uf1/Els tipus de variables primitius/index.php
@@ -0,0 +1,21 @@
+<?php
+$i = 12;
+$tipus_de_i = gettype($i);
+echo "La variable \$i 
+      conté el valor $i 
+	  i és del tipus $tipus_de_i<br>";
+$i = 12.01;
+$tipus_de_i = gettype($i);
+echo "La variable \$j 
+      conté el valor $i 
+	  i és del tipus $tipus_de_i<br>";
+$i = true;
+$tipus_de_i = gettype($i);
+echo "La variable \$k 
+      conté el valor $i 
+	  i és del tipus $tipus_de_i<br>";
+$i = "12";
+$tipus_de_i = gettype($i);
+echo "La variable \$l 
+      conté el valor $i 
+	  i és del tipus $tipus_de_i<br>";
---
+++ uf1/L'Array associatiu $_SERVER/index.php
@@ -0,0 +1,5 @@
+<?php
+//pitem el contingut de l'array
+foreach ($_SERVER as $clau => $valor) {
+    echo "El valor de [$clau] és [$valor] <br>";
+}
