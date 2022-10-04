4c284e492b8e91eac5e5415033feea599059b884
NilPujol <80785969+NilPujol@users.noreply.github.com>
2022-10-05 00:07:44 +0200

    Update index.php

--- uf1/Pràctiac A2 - Calculadora/index.php
+++ uf1/Pràctiac A2 - Calculadora/index.php
@@ -1,90 +1,68 @@
+<?php 
+    $valor = "";
+    if(isset($_POST['valor'])){
+        $valor = $_REQUEST['valor'];
+        $valor = $_REQUEST['set'].$valor;
+    }elseif(isset($_POST['igual'])){
+        $valor = verificarOperacio();
+    }
+    function ferOperacio() {
+        $valor = ltrim($_POST['set'],0);
+        eval("\$valor2=$valor;");        
+        return $valor2;
+    } 
+    function verificarOperacio() {
+        $regex = "/^[0-9\(\)-.\*\+\/cosin]*$/";
+        if(preg_match($regex,$_POST['set'])){
+            try{
+                $valor = round(ferOperacio(),4);//round es per arrodonir a 4 decimals
+            }
+            catch(DivisionByZeroError $e){
+                $valor = "Inf";
+            }
+            catch(Throwable $e){
+                $valor = "ERROR";
+            }
+        }else{
+            $valor = "ERROR";
+        }
+        return $valor;
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="ca">
-
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link rel="stylesheet" type="text/css" href="style.css" />
     <title>Calculadora</title>
 </head>
-<?php
-$valor = "";
-if (isset($_GET['valor'])) {
-    $valor = $_GET['valor'];
-}
-$output = "0";
-if (isset($_GET['output'])) {
-    $output = $_GET['output'];
-}
-if ($output == "0" || $output == "00") {
-    if ($valor != "") {
-        $output = "";
-    }
-    if ($valor == "=") {
-        $valor = "";
-        $output = "0";
-    }
-}
-if ($valor == "C") {
-    $output = "0";
-    $valor = "";
-} else if ($valor == "=") {
-    $valor = checkOperation($output, "");
-
-    if ($valor != "ERROR") {
-        ltrim($valor);
-        try {
-            eval("\$output=$valor;");
-            $output = round($output, 4);
-        } catch (DivisionByZeroError $e) {
-            $output = "inf";
-        } catch (Throwable) {
-            $output = "ERROR";
-        }
-        $valor = "";
-    } else {
-        $output = "";
-    }
-}
-?>
-
 <body>
     <div class="container">
-        <form name="calc" class="calculator" method="get">
-            <input type="text" class="value" readonly name="output" value="<?php
-                                                                            echo checkOperation($output, $valor); ?>" />
-            <span class="num clear"><input type="submit" value="C" name="valor"></span>
-            <span class="num"><input type="submit" value="/" name="valor"></span>
-            <span class="num"><input type="submit" value="*" name="valor"></span>
-            <span class="num"><input type="submit" value="7" name="valor"></span>
-            <span class="num"><input type="submit" value="8" name="valor"></span>
-            <span class="num"><input type="submit" value="9" name="valor"></span>
-            <span class="num"><input type="submit" value="-" name="valor"></span>
-            <span class="num"><input type="submit" value="4" name="valor"></span>
-            <span class="num"><input type="submit" value="5" name="valor"></span>
-            <span class="num"><input type="submit" value="6" name="valor"></span>
-            <span class="num plus"><input type="submit" value="+" name="valor"></span>
-            <span class="num"><input type="submit" value="1" name="valor"></span>
-            <span class="num"><input type="submit" value="2" name="valor"></span>
-            <span class="num"><input type="submit" value="3" name="valor"></span>
-            <span class="num"><input type="submit" value="0" name="valor"></span>
-            <span class="num"><input type="submit" value="00" name="valor"></span>
-            <span class="num"><input type="submit" value="." name="valor"></span>
-            <span class="num equal"><input type="submit" value="=" name="valor"></span>
+        <form name="calc" class="calculator" action="index.php" method="POST">
+            <input type="text" class="value" name="set" readonly value="<?php echo $valor; ?>"/>
+            <span class="num"><input type ="submit" value="(" name="valor"></span>
+            <span class="num"><input type ="submit" value=")" name="valor"></span>
+            <span class="num"><input type ="submit" value="sin" name="valor"></span>
+            <span class="num"><input type ="submit" value="cos" name="valor"></span>
+            <span class="num clear"><input type ="submit" value="C" name="borrar"></span>
+            <span class="num"><input type ="submit" value="/" name="valor"></span>
+            <span class="num"><input type ="submit" value="*" name="valor"></span>
+            <span class="num"><input type ="submit" value="7" name="valor"></span>
+            <span class="num"><input type ="submit" value="8" name="valor"></span>
+            <span class="num"><input type ="submit" value="9" name="valor"></span>
+            <span class="num"><input type ="submit" value="-" name="valor"></span>
+            <span class="num"><input type ="submit" value="4" name="valor"></span>
+            <span class="num"><input type ="submit" value="5" name="valor"></span>
+            <span class="num"><input type ="submit" value="6" name="valor"></span>
+            <span class="num plus"><input type ="submit" value="+" name="valor"></span>
+            <span class="num"><input type ="submit" value="1" name="valor"></span>
+            <span class="num"><input type ="submit" value="2" name="valor"></span>
+            <span class="num"><input type ="submit" value="3" name="valor"></span>
+            <span class="num"><input type ="submit" value="0" name="valor"></span>
+            <span class="num"><input type ="submit" value="00" name="valor"></span>
+            <span class="num"><input type ="submit" value="." name="valor"></span>
+            <span class="num equal"><input type ="submit" value="="name="igual"></span>
         </form>
     </div>
 </body>
-
-<?php
-function checkOperation($output, $valor)
-{
-
-
-
-    $str = $output . $valor;
-    $regex = "/^(cos)|(sin)|(inf)|[0-9\(\)\-.\*\+\/]*$/";
-    if (preg_match($regex, $str)) {
-        return $str;
-    } else {
-        return "ERROR";
-    }
-}
